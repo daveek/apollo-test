d698b3eddb6ea3f137cc4aa788d0e92286584058
David Zukowski <zukoweb@gmail.com>
2017-12-14 19:38:55 -0600

    feat(graphiql): add route for GraphQL query explorer

--- server/src/index.js
+++ server/src/index.js
@@ -4,8 +4,12 @@ const ApolloServer = require('apollo-server-express')
 const app = express()
 
 app.use(require('body-parser').json())
+
 app.use('/graphql', ApolloServer.graphqlExpress({
   schema: require('./schema'),
+}))
+app.use('/graphiql', ApolloServer.graphiqlExpress({
+  endpointURL: '/graphql',
 }))
 
 module.exports = app
