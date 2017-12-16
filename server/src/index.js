a022b59da98cec482953386a0a9171127b60f047
David Zukowski <zukoweb@gmail.com>
2017-12-15 23:49:48 -0600

    feat(server): add morgan logger

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const ApolloServer = require('apollo-server-express')
 
 const app = express()
 
+app.use(require('morgan')('dev'))
 app.use(require('body-parser').json())
 
 app.use('/graphql', ApolloServer.graphqlExpress({
