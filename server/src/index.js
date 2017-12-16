6769fa0c957434732b1ff5ee602d84a4e1f3a20b
David Zukowski <zukoweb@gmail.com>
2017-12-15 23:50:07 -0600

    feat(server): add default error handler

--- server/src/index.js
+++ server/src/index.js
@@ -13,4 +13,10 @@ app.use('/graphiql', ApolloServer.graphiqlExpress({
   endpointURL: '/graphql',
 }))
 
+app.use((err, req, res, next) => {
+  if (res.headersSent) return next(err)
+  res.status(err.status || 500)
+  res.json({ error: err.message })
+})
+
 module.exports = app
