fd05b11b5ab2519d313ec34aceb9438dad4219e2
Levi Thomason <me@levithomason.com>
2017-12-27 13:55:51 -0800

    feat(client): scaffold chat interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,84 @@
+import faker from 'faker'
+import _ from 'lodash/fp'
+import moment from 'moment'
 import React, { Component } from 'react'
-import { Container, Image, Menu, Segment } from 'semantic-ui-react'
+import { Form, Feed, Grid, Header, List } from 'semantic-ui-react'
+
+const noFlex = { flex: '0 0 auto' }
+
+const sidebarStyle = {
+  ...noFlex,
+  width: '15em',
+}
+
+const sidebarHeaderStyle = {
+  ...noFlex,
+}
+
+const gridStyle = {
+  height: '100vh',
+}
+
+const inputFormStyle = {
+  ...noFlex,
+}
+
+const feedStyle = {
+  margin: 0,
+  overflowY: 'auto',
+  paddingBottom: '1em',
+}
+
+const textAreaStyle = {
+  border: '2px solid grey',
+}
 
 class App extends Component {
+  getChannelItem = key => ({
+    key,
+    as: 'a',
+    icon: 'hashtag',
+    content: faker.hacker.noun(),
+  })
+
+  getChannelList = () => _.times(this.getChannelItem, 10)
+
+  getChatMessage = index => ({
+    key: index,
+    image: faker.image.avatar(),
+    summary: {
+      content: faker.internet.userName(),
+      date: moment(Date.now() - index * 100000).fromNow(),
+    },
+    extraText: faker.hacker.phrase(),
+  })
+
+  getChatMessages = () => _.times(this.getChatMessage, 20).reverse()
+
   render() {
     return (
-      <Container>
-        <Menu>
-          <Menu.Item header>
-            <Image avatar src="//unsplash.it/100" />
-            Welcome to React
-          </Menu.Item>
-        </Menu>
-        <Segment>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </Segment>
-      </Container>
+      <Grid columns="equal" padded stretched style={gridStyle}>
+        <Grid.Column color="blue" style={sidebarStyle}>
+          <Header
+            inverted
+            icon="code"
+            content="Data store"
+            style={sidebarHeaderStyle}
+          />
+          <List inverted link items={this.getChannelList()} />
+        </Grid.Column>
+        <Grid.Column>
+          <Feed events={this.getChatMessages()} style={feedStyle} />
+          <Form style={inputFormStyle}>
+            <Form.TextArea
+              style={textAreaStyle}
+              autoHeight
+              rows={1}
+              placeholder="Message channel..."
+            />
+          </Form>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
