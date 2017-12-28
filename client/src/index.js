dc15cd0ccd4ed0f9f94e1b07de1269f5725b62b2
Levi Thomason <me@levithomason.com>
2017-12-28 15:49:25 -0800

    feat(client): stub data-client

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
+import { client } from './data-client'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
